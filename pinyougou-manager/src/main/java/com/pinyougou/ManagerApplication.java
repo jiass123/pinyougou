29c05984c0ccf0c08e4f71a1aa3bb58dd373330a
jiass <jiass@asiainfo.com>
2019-02-22 00:03:50 +0800

    123

--- pinyougou-manager/src/main/java/com/pinyougou/ManagerApplication.java
+++ pinyougou-manager/src/main/java/com/pinyougou/ManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PiyougouManagerApplication {
+public class ManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PiyougouManagerApplication.class, args);
+        SpringApplication.run(ManagerApplication.class, args);
     }
 
 }
